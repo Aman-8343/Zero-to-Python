a4b295d7d0619e5e8672067b9f205c5811a75978
Aman kumar <amankumar105541@gmail.com>
2025-11-16 14:27:36 +0530

    accumulation of number

---
+++ Basic/accumulation.py
@@ -0,0 +1,11 @@
+ #concationation of str
+a=input("enter no ")
+b=input("enter no.2 ")
+sum =a+b
+print(sum)
+
+
+a=int(input("enter no "))
+b=int(input("enter no.2 "))
+sum =a+b
+print("sum is ",sum)
