62497e650b3113cf0e73200f2004b5bb80ad5fda
Aman kumar <amankumar105541@gmail.com>
2025-11-16 14:06:47 +0530

    Operators

---
+++ Basic/Operator.py
@@ -0,0 +1,24 @@
+a=5
+b=4
+
+#arithmethic
+print(a+b)
+print(a/b)
+print(a%b)
+print(a**b)
+
+#relational
+print(a>=b)
+print(a==b)
+
+#assigment
+a+=5
+b*=3
+print(a)
+print(b)
+
+#logical
+ans=False
+print(not ans)
+print((5>2) and (6>2))
+print((7<4) or (7<8))
--- Basic/hello.py
+++ Basic/hello.py
@@ -5,3 +5,4 @@ pi=3.14
 is_student=True
 print(name, age , pi , is_student)
 print(type(pi))
+print(type(name));
