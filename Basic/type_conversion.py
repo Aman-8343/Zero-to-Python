6047c6fa20dcffd4354d768f5d25cf9fb2929f6f
Aman kumar <amankumar105541@gmail.com>
2025-11-16 14:19:52 +0530

    type conversion and casting

---
+++ Basic/type_conversion.py
@@ -0,0 +1,17 @@
+#conversion - implict
+val=5+10
+val2=5+7.8
+
+print(type(val))
+print(type(val2))
+
+
+#casting-explict
+val3=float(5+10)
+val4=int(5+7.8)
+
+print(val3,type(val3))
+print(val4,type(val4))
+
+ans=bool(8)
+print(ans,type(ans))
