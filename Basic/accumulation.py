c5b419ae29625d2f3862e29eef0f37b1cf64fb3c
Aman kumar <amankumar105541@gmail.com>
2025-11-16 14:38:41 +0530

    avg of no

--- Basic/accumulation.py
+++ Basic/accumulation.py
@@ -4,8 +4,14 @@ b=input("enter no.2 ")
 sum =a+b
 print(sum)
 
-
+#additon
 a=int(input("enter no "))
 b=int(input("enter no.2 "))
 sum =a+b
 print("sum is ",sum)
+
+#average
+a=int(input("1st no. "))
+b=int(input("2nd no. "))
+avg=float((a+b)/2)
+print("average is : ",avg)
