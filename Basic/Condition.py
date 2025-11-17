9b00948c6153b2dffa31bb0f58d83941638acbe0
Aman kumar <amankumar105541@gmail.com>
2025-11-17 14:26:43 +0530

    Refactor age classification logic and improve user input prompts

--- Basic/Condition.py
+++ Basic/Condition.py
@@ -1,7 +1,19 @@
-age=45
+'''
+age =int(input("Enter your age "))
 
+if age<13:
+    print("child")
 
-if age>=18 :
-    print("Hero")
+elif age>13 and age<25 :
+    print("mature")
 
-else :  print("zero")
+else : print("adult")
+'''
+
+username=input("Enter usename")
+password=input("Enter passoword")
+
+if (username=="admin" and password=="password") :
+    print("logged in")
+
+else : print("wrong info")    
