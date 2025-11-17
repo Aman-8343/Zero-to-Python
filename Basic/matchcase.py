929ac8e246ca17262568df92cbb5aee24c16f499
Aman kumar <amankumar105541@gmail.com>
2025-11-17 16:13:00 +0530

    match case

---
+++ Basic/matchcase.py
@@ -0,0 +1,10 @@
+color=input("ENter the color ")
+
+match color:
+    case "red" :
+        print("go")
+    case "yellow":
+        print("look")
+    case "green":
+        print ("g0")   
+    case _: print("wrong color")     
