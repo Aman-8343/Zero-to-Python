46fd87e441d475231e6b28b94a5780c4ce5083e6
Aman kumar <amankumar105541@gmail.com>
2025-11-17 14:07:40 +0530

    ...

---
+++ Basic/Condition.py
@@ -0,0 +1,7 @@
+age=45
+
+
+if age>=18 :
+    print("Hero")
+
+else :  print("zero")
---
+++ Basic/qs.py
@@ -0,0 +1,47 @@
+#swap 2 no
+'''
+a=input("enter 1st no ")
+b=input("enter 2st no ")
+
+temp=a
+a=b
+b=temp
+
+print(" number after swapping is : a= ",a ,"b= ",b)
+'''
+
+#celcius to fahren
+'''
+temp=float(input("Enter temp in celcius "))
+
+fahrentemp= (temp*(9/5))+32
+print("temp in fahren is", fahrentemp)
+'''
+
+
+#AREA OF CIRCLE
+'''
+pi=3.14
+r=float(input(" Enter the radius "))
+area= pi*r*r
+print("area of circle is ", area)
+'''
+ # simple interest
+'''
+p=float(input("enter p"))
+r=float(input("enter r"))
+t=float(input("enter t"))
+si=p*r*t*100;
+print("simple interest is ",si)
+'''
+
+
+no= float(input("no : "))
+
+no2=int(no)
+print("integer part is :",no2)
+
+no3=no-no2
+print("fractional part is :",no3)
+
+
