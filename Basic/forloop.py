fac059fe01cf2ae73e2eedc372474310f32070d8
Aman kumar <amankumar105541@gmail.com>
2025-11-18 17:06:01 +0530

    ...

---
+++ Basic/forloop.py
@@ -0,0 +1,26 @@
+
+
+string ="amankumar"
+
+#in - memborship operator
+'''
+for k in string:
+    print(k)
+
+if  'k' in string :
+    print("exist")    
+
+for i in range(8)  :
+    print(i)  
+    '''
+
+
+#vowel count
+word="artifical intelligence"
+ans=0
+for i in word:
+    if(i=='a' or i=='e' or i=='o' or i=='i' or i=='u'):
+       ans+=1
+
+print("ans =",ans)
+
