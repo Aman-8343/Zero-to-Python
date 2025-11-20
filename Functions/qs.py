2539b6b3227b1f680c8daec9b7fc1403514a71f6
Aman kumar <amankumar105541@gmail.com>
2025-11-20 22:25:27 +0530

    prime no.

--- Functions/qs.py
+++ Functions/qs.py
@@ -60,4 +60,14 @@
 
 
 
+# prime no.
+def is_prime(n):
+    count=0
+    for i in range(1,n+1):
+        if n%i==0:
+            count+=1
+    if count==2:
+            return True
+    else: return False
 
+print(is_prime(11))      
