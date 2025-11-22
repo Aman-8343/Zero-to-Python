084c2a6308047bbd0133de7f660a16e053ee98c6
Aman kumar <amankumar105541@gmail.com>
2025-11-22 16:21:39 +0530

    update method

--- Dictionary/methods.py
+++ Dictionary/methods.py
@@ -14,3 +14,12 @@ print(info.values())
 items=info.items()
 print(items)   #return all key value pairs
 
+
+print(info.get("subject"))
+print(info.get("subjectsss"))   # return none if no key exists
+
+
+info.update({
+    "city":"djfd"
+})
+print(info)
