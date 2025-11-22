0b9b295ee2d10880d2ae0c3df3ba12731928e3c7
Aman kumar <amankumar105541@gmail.com>
2025-11-22 22:46:50 +0530

    sets and its methods

---
+++ Sets/methods.py
@@ -0,0 +1,17 @@
+s={1,33,4,3,3,3,5}
+
+s.add(8)
+s.remove(3)
+print(s)
+
+  #s.clear();  clear the set
+
+s.pop();    #removes random values
+print(s)
+
+
+s2={3,43,53,5,33,43}
+
+
+print(s.intersection(s2))
+print(s.union(s2))
---
+++ Sets/sets.py
@@ -0,0 +1,7 @@
+s={1,33,4,3,3,3,5}
+
+print(s);
+print(type(s))
+
+empty_set=set();
+print(type(empty_set))
