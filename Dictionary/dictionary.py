d8a60a03fe41653e65710094a8abd37ac794214e
Aman kumar <amankumar105541@gmail.com>
2025-11-22 16:08:43 +0530

    add dictionary example with key-value pairs

---
+++ Dictionary/dictionary.py
@@ -0,0 +1,12 @@
+# Dictionary stores data in key value pair and all keys are unique
+#mutable in nature
+
+info ={
+    "name":"hero",
+    "cgpa":10,
+    3.14:"pi",
+    "subject":["eng","chem"]
+}
+
+info["name"]="hero2"
+print(info["name"])
