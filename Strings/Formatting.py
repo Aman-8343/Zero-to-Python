4b4fb5b0d0c27a2f91e1c08c8e56a081ed537331
Aman kumar <amankumar105541@gmail.com>
2025-11-22 15:15:23 +0530

    value based

--- Strings/Formatting.py
+++ Strings/Formatting.py
@@ -10,3 +10,6 @@ print("sum of {} and {} is {}".format(a,b,sum))
 #index based formatting
 
 print("sum of {1} and {0} is {2}".format(a,b,sum))
+
+#value based formatting
+print("val of {a} and {b}".format(a=5,b=7))
