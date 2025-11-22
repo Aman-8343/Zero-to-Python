52d1cd5350c167901e45685a51b48aecb35fc222
Aman kumar <amankumar105541@gmail.com>
2025-11-22 15:13:09 +0530

    index formatting

--- Strings/Formatting.py
+++ Strings/Formatting.py
@@ -6,3 +6,7 @@ sum=a+b;
 print("lang is {}".format("python"))
 print("sum is {}".format(sum))
 print("sum of {} and {} is {}".format(a,b,sum))
+
+#index based formatting
+
+print("sum of {1} and {0} is {2}".format(a,b,sum))
