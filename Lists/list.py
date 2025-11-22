a7c09ba5384e57a5d884cd6494c65a1d9e62872c
Aman kumar <amankumar105541@gmail.com>
2025-11-22 15:28:33 +0530

    slicing

--- Lists/list.py
+++ Lists/list.py
@@ -1,7 +1,12 @@
-marks=[34,34,65,7,5,34]
+marks=[34,34,65,7,5,34,"abd",5]
 print(marks[2]);
 print(len(marks));
+print(type(marks))
 
 #list are mutable
 marks[5]=23
 print (marks)
+
+#list slicing
+print(marks[3:5])
+print(marks[5:])
