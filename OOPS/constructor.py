72807f29423a7fdbc3c0e636a818a7ef3905fa9c
Aman kumar <amankumar105541@gmail.com>
2025-11-27 22:31:22 +0530

    Fix student class constructor to accept parameters for name and GPA

--- OOPS/constructor.py
+++ OOPS/constructor.py
@@ -1,7 +1,10 @@
 class student :
-    def __init__ (self):
-        print("constructor is called ...")
+    def __init__ (self,name,gpa):
+        self.namee=name;
+        self.gpa=gpa;
+        
 
 
-stu1=student();
+stu1=student("aman",9.8);
+print(stu1.namee,stu1.gpa);
 
