7447f85f7436e62b8c0aca7a0567c71a18da5168
Aman kumar <amankumar105541@gmail.com>
2025-11-28 13:04:35 +0530

    Fix typo in student class attribute name and clean up print statements

--- OOPS/constructor.py
+++ OOPS/constructor.py
@@ -1,10 +1,19 @@
 class student :
     def __init__ (self,name,gpa):
-        self.namee=name;
+        self.name=name;
         self.gpa=gpa;
+
+    
+    def get_cgpa(self):
+        return self.gpa
         
 
 
-stu1=student("aman",9.8);
-print(stu1.namee,stu1.gpa);
+stu1=student("aman",9.8)
+print(stu1.name,stu1.gpa)
+
+stu2=student("raj",6.6)
+
+print(f"{stu1.name} cgpa is {stu1.get_cgpa()}")
+print(stu1.get_cgpa())
 
