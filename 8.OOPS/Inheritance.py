da5d86c3bd603f646dcbb5ecf779116afdae546f
Aman kumar <amankumar105541@gmail.com>
2025-11-28 17:12:09 +0530

    multiple inheritance

--- 8.OOPS/Inheritance.py
+++ 8.OOPS/Inheritance.py
@@ -26,6 +26,7 @@ print(t1.name,t1.st_time)
 
 
 #multilevel inheritance
+'''
 class emp:
     st_time="5"
     end_time="7"
@@ -41,3 +42,25 @@ class accountant(admin):
 
 acc1=accountant("3434","ca")
 print(acc1.salary,acc1.role)
+'''
+
+#multiple inheritance
+class teacher :
+    def __init__(self,salary):
+        self.salary=salary
+ 
+
+class student :
+    def __init__(self,gpa):
+        self.gpa=gpa
+
+
+class TA(teacher,student):
+    def __init__(self, salary,gpa,name):
+        super().__init__(salary)
+        student.__init__(self,gpa)
+        self.name=name
+
+ta1=TA(3433,9,"aman")
+print(ta1.salary,ta1.gpa,ta1.name)
+
