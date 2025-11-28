b56003aa4edb5c808b6f56f581b73d507f69514e
Aman kumar <amankumar105541@gmail.com>
2025-11-28 21:47:45 +0530

    duck typing

--- 8.OOPS/Polymorphism.py
+++ 8.OOPS/Polymorphism.py
@@ -15,3 +15,21 @@ a1.get_design()
 
 e1=emp()
 e1.get_design()
+
+
+#duck Typing -> if it walk like a duck and quacks like a duck , it is a duck
+
+class pycharm:
+    def execute(self):
+        print("compiling")
+          
+
+class laptop:
+    def execute(self,):
+        print("running")
+
+l1=laptop()
+l1.execute()
+
+p1=pycharm()
+p1.execute()
