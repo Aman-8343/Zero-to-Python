160be0cb1346bfe9dbc117a419c1dc0dfcf03fb1
Aman kumar <amankumar105541@gmail.com>
2025-11-29 21:00:32 +0530

    with keyword

--- 9.FileIO/operation.py
+++ 9.FileIO/operation.py
@@ -1,5 +1,15 @@
+'''
 f=open("data.txt","w")
 
 f.write("overwrittten text")
 
 f.close()
+'''
+
+#with - open file and automatically close after block
+
+with open("sample.txt","r") as f:
+    data=f.read()
+    print(data)
+    print(len(data))
+
