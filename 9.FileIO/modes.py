47233503fa3f71f076bc4d66517a490c4cd8086d
Aman kumar <amankumar105541@gmail.com>
2025-11-29 16:29:02 +0530

    add file I/O operations to create sample2.txt and append text to sample.txt

--- 9.FileIO/modes.py
+++ 9.FileIO/modes.py
@@ -4,3 +4,10 @@ f=open("sample.txt","a")
 f.write("new appended text")
 
 f.close()
+
+#x->creates new file and open for writing
+f=open("sample2.txt","x")
+
+f.write("new appended text")
+
+f.close()
