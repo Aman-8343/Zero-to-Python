5a3f1b4a6d62412efaba9922a49b422f73d3cf87
Aman <amankumar105541@gmail.com>
2026-01-28 14:39:45 +0530

    update file  io

--- 10.Module/code.py
+++ 10.Module/code.py
@@ -12,7 +12,6 @@ py_obj= json.loads(json_str)   #convert json string to python dict
 
 strr= json.dumps(py_dict)   #convert python dict to json string
 
-print(py_obj)
 print(type(py_obj))
 
 print(strr,type(strr))
